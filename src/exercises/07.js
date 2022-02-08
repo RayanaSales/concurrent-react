1a05860f094ecdb347be01206b3a8a9d9f44f151
Rayana Sales <rayana.sales@thoughtworks.com>
2022-02-07 22:03:57 -0300

    exercise 07 - Use SuspenseList for Coordinating Suspending Components

--- src/exercises/07.js
+++ src/exercises/07.js
@@ -1,7 +1,3 @@
-// Coordinate Suspending components with SuspenseList
-
-// http://localhost:3000/isolated/exercises/07
-
 import React from 'react'
 import '../suspense-list/style-overrides.css'
 import * as cn from '../suspense-list/app.module.css'
@@ -9,12 +5,9 @@ import Spinner from '../suspense-list/spinner'
 import {createResource, ErrorBoundary, PokemonForm} from '../utils'
 import {fetchUser} from '../fetch-pokemon'
 
-// 💰 this delay function just allows us to make a promise take longer to resolve
-// so we can easily play around with the loading time of our code.
 const delay = time => promiseResult =>
   new Promise(resolve => setTimeout(() => resolve(promiseResult), time))
 
-// 🐨 feel free to play around with the delay timings.
 const NavBar = React.lazy(() =>
   import('../suspense-list/nav-bar').then(delay(500)),
 )
@@ -22,7 +15,7 @@ const LeftNav = React.lazy(() =>
   import('../suspense-list/left-nav').then(delay(2000)),
 )
 const MainContent = React.lazy(() =>
-  import('../suspense-list/main-content').then(delay(1500)),
+  import('../suspense-list/main-content').then(delay(5000)),
 )
 const RightNav = React.lazy(() =>
   import('../suspense-list/right-nav').then(delay(1000)),
@@ -53,42 +46,32 @@ function App() {
     )
   }
 
-  // 🐨 Use React.SuspenseList throughout these Suspending components to make
-  // them load in a way that is not jaring to the user.
-  // 💰 there's not really a specifically "right" answer for this.
   return (
     <div className={cn.root}>
       <ErrorBoundary>
-        <React.Suspense fallback={fallback}>
-          <NavBar pokemonResource={pokemonResource} />
-        </React.Suspense>
-        <div className={cn.mainContentArea}>
+        <React.SuspenseList revealOrder="forwards" tail="collapsed">
           <React.Suspense fallback={fallback}>
-            <LeftNav />
+            <NavBar pokemonResource={pokemonResource} />
           </React.Suspense>
-          <React.Suspense fallback={fallback}>
-            <MainContent pokemonResource={pokemonResource} />
-          </React.Suspense>
-          <React.Suspense fallback={fallback}>
-            <RightNav pokemonResource={pokemonResource} />
-          </React.Suspense>
-        </div>
+          <div className={cn.mainContentArea}>
+            <React.SuspenseList revealOrder="forwards" tail="collapsed">
+              <React.Suspense fallback={fallback}>
+                <LeftNav />
+              </React.Suspense>
+              <React.SuspenseList revealOrder="together">
+                <React.Suspense fallback={fallback}>
+                  <MainContent pokemonResource={pokemonResource} />
+                </React.Suspense>
+                <React.Suspense fallback={fallback}>
+                  <RightNav pokemonResource={pokemonResource} />
+                </React.Suspense>
+              </React.SuspenseList>
+            </React.SuspenseList>
+          </div>
+        </React.SuspenseList>
       </ErrorBoundary>
     </div>
   )
 }
-
-/*
-🦉 Elaboration & Feedback
-After the instruction, copy the URL below into your browser and fill out the form:
-http://ws.kcd.im/?ws=Concurrent%20React&e=Coordinate%20Suspending%20components%20with%20SuspenseList&em=
-*/
-
-////////////////////////////////////////////////////////////////////
-//                                                                //
-//                 Don't make changes below here.                 //
-// But do look at it to see how your code is intended to be used. //
-//                                                                //
-////////////////////////////////////////////////////////////////////
 
 export default App
